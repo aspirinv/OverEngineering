527acab5e9c4edd04b1a01eeab71358676f73773
aspirinv <aspirin.gm@gmail.com>
2020-11-18 16:54:27 +0100

    3. Separating parser

--- OverEngineering/Controllers/WaterController.cs
+++ OverEngineering/Controllers/WaterController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OverEngineering.DTO;
+using OverEngineering.Logic;
 
 namespace OverEngineering.Controllers
 {
@@ -22,47 +19,20 @@ namespace OverEngineering.Controllers
             to ??= DateTime.Today;
             // Defining URLs
             var query = $"beginn={from:dd.MM.yyyy}&ende={to:dd.MM.yyyy}";
-            var baseUrl = new Uri("https://www.gkd.bayern.de/de/fluesse/");
-            using (var client = new HttpClient { BaseAddress = baseUrl })
+            using var parser = new MeasureParser(query);
+            try
             {
-                try
+                // Collecting data
+                return Ok(new MeasurementSet
                 {
-                    // Collecting data
-                    return Ok(new MeasurementSet
-                    {
-                        Temperature = await GetMeasures(query, client, "wassertemperatur"),
-                        Level = await GetMeasures(query, client, "wasserstand"),
-                    });
-                }
-                catch (Exception ex)
-                {
-                    return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-                }
+                    Temperature = await parser.GetMeasures(MeasureType.Temperature),
+                    Level = await parser.GetMeasures(MeasureType.Level),
+                });
             }
-        }
-
-        private static async Task<IEnumerable<Measurement>> GetMeasures(string query, HttpClient client, string apiName)
-        {
-            // Retrieving the data
-            var response = await client.GetAsync($"{apiName}/isar/muenchen-tieraerztl-hochschule-16516008/messwerte/tabelle?{query}");
-            var html = await response.Content.ReadAsStringAsync();
-            if (response.IsSuccessStatusCode)
+            catch (Exception ex)
             {
-                throw new Exception($"{apiName} gathering failed with. [{response.StatusCode}] {html}");
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
-            // Parsing HTML response
-            var bodyMatch = Regex.Match(html, "<tbody>(.*)<\\/tbody>");
-            if (!bodyMatch.Success)
-            {
-                throw new Exception($"Failed to define data table body. Content: {html}");
-            }
-
-            var rowsHtml = bodyMatch.Groups.Values.Last();
-            return Regex.Matches(rowsHtml.Value, "<tr  class=\"row2?\"><td >([^<]*)<\\/td><td  class=\"center\">([^<]*)<\\/td>")
-                // Building the results
-                .Select(match => Measurement.Parse(match.Groups[1].Value, match.Groups[2].Value))
-                // Ignoring invalid values
-                .Where(m => m != null);
         }
     }
 }
---
+++ OverEngineering/Logic/MeasureParser.cs
@@ -0,0 +1,59 @@
+﻿using OverEngineering.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OverEngineering.Logic
+{
+    public class MeasureParser : IDisposable
+    {
+        private static Uri BaseUrl = new Uri("https://www.gkd.bayern.de/de/fluesse/");
+        private readonly string _query;
+        private readonly HttpClient _client;
+
+        public MeasureParser(string query)
+        {
+            _query = query;
+            _client = new HttpClient { BaseAddress = BaseUrl };
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+
+        public async Task<IEnumerable<Measurement>> GetMeasures(MeasureType measure)
+        {
+            var apiName = measure switch
+            {
+                MeasureType.Temperature => "wassertemperatur",
+                MeasureType.Level => "wasserstand",
+                _ => throw new NotImplementedException($"Measure type {measure} not implemented")
+            };
+
+            // Retrieving the data
+            var response = await _client.GetAsync($"{apiName}/isar/muenchen-tieraerztl-hochschule-16516008/messwerte/tabelle?{_query}");
+            var html = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                throw new Exception($"{apiName} gathering failed with. [{response.StatusCode}] {html}");
+            }
+            // Parsing HTML response
+            var bodyMatch = Regex.Match(html, "<tbody>(.*)<\\/tbody>");
+            if (!bodyMatch.Success)
+            {
+                throw new Exception($"Failed to define data table body. Content: {html}");
+            }
+
+            var rowsHtml = bodyMatch.Groups.Values.Last();
+            return Regex.Matches(rowsHtml.Value, "<tr  class=\"row2?\"><td >([^<]*)<\\/td><td  class=\"center\">([^<]*)<\\/td>")
+                // Building the results
+                .Select(match => Measurement.Parse(match.Groups[1].Value, match.Groups[2].Value))
+                // Ignoring invalid values
+                .Where(m => m != null);
+        }
+    }
+}
