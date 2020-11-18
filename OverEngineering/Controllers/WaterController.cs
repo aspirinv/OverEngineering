f3402dcf1291c08f14eb679274493025455c2c93
aspirinv <aspirin.gm@gmail.com>
2020-11-18 16:44:07 +0100

    2. Parsing errors handling

--- OverEngineering/Controllers/WaterController.cs
+++ OverEngineering/Controllers/WaterController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OverEngineering.DTO;
 
@@ -14,7 +15,7 @@ namespace OverEngineering.Controllers
     public class WaterController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<MeasurementSet>> Get([FromQuery]DateTime? from = null, [FromQuery]DateTime? to = null)
+        public async Task<ActionResult<MeasurementSet>> Get([FromQuery] DateTime? from = null, [FromQuery] DateTime? to = null)
         {
             // Defaulting values
             from ??= DateTime.Today.AddDays(-1);
@@ -24,12 +25,19 @@ namespace OverEngineering.Controllers
             var baseUrl = new Uri("https://www.gkd.bayern.de/de/fluesse/");
             using (var client = new HttpClient { BaseAddress = baseUrl })
             {
-                // Collecting data
-                return Ok(new MeasurementSet 
-                { 
-                    Temperature = await GetMeasures(query, client, "wassertemperatur"), 
-                    Level = await GetMeasures(query, client, "wasserstand"), 
-                });
+                try
+                {
+                    // Collecting data
+                    return Ok(new MeasurementSet
+                    {
+                        Temperature = await GetMeasures(query, client, "wassertemperatur"),
+                        Level = await GetMeasures(query, client, "wasserstand"),
+                    });
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+                }
             }
         }
 
@@ -38,16 +46,23 @@ namespace OverEngineering.Controllers
             // Retrieving the data
             var response = await client.GetAsync($"{apiName}/isar/muenchen-tieraerztl-hochschule-16516008/messwerte/tabelle?{query}");
             var html = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                throw new Exception($"{apiName} gathering failed with. [{response.StatusCode}] {html}");
+            }
             // Parsing HTML response
             var bodyMatch = Regex.Match(html, "<tbody>(.*)<\\/tbody>");
+            if (!bodyMatch.Success)
+            {
+                throw new Exception($"Failed to define data table body. Content: {html}");
+            }
+
             var rowsHtml = bodyMatch.Groups.Values.Last();
             return Regex.Matches(rowsHtml.Value, "<tr  class=\"row2?\"><td >([^<]*)<\\/td><td  class=\"center\">([^<]*)<\\/td>")
                 // Building the results
-                .Select(match => new Measurement
-                {
-                    Date = DateTime.Parse(match.Groups[1].Value),
-                    Value = decimal.Parse(match.Groups[2].Value)
-                });
+                .Select(match => Measurement.Parse(match.Groups[1].Value, match.Groups[2].Value))
+                // Ignoring invalid values
+                .Where(m => m != null);
         }
     }
 }
--- OverEngineering/DTO/Measurement.cs
+++ OverEngineering/DTO/Measurement.cs
@@ -9,5 +9,14 @@ namespace OverEngineering.DTO
     {
         public DateTime Date { get; set; }
         public decimal Value { get; set; }
+
+        public static Measurement Parse(string date, string value)
+        {
+            if (DateTime.TryParse(date, out var d) && decimal.TryParse(value, out var v))
+            {
+                return new Measurement { Date = d, Value = v };
+            }
+            return null;
+        }
     }
 }
