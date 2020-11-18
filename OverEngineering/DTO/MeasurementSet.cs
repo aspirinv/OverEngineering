da502a5bf99d615f240ad1d70d8acae26e9fef7c
aspirinv <aspirin.gm@gmail.com>
2020-11-18 14:25:38 +0100

    Add project files.

---
+++ OverEngineering/DTO/MeasurementSet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OverEngineering.DTO
+{
+    public class MeasurementSet
+    {
+        public IEnumerable<Measurement> Temperature { get; set; }
+        public IEnumerable<Measurement> Level { get; set; }
+    }
+}
