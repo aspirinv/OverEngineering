5c632c90afec7694454ee5c813e978822560bed8
aspirinv <aspirin.gm@gmail.com>
2020-11-27 09:23:54 +0100

    Corrected test checks

--- Tests/UrlQueryBuilderTests.cs
+++ Tests/UrlQueryBuilderTests.cs
@@ -16,7 +16,7 @@ namespace Tests
                 To = DateTime.Parse("2020-11-24T00:00:00")
             };
             var actual = builder.BuildLevelQuery();
-            var expected = "de/fluesse/wasserstand/isar/muenchen-tieraerztl-hochschule-16516008/messwerte/tabelle?beginn=23.11.2020&ende=24.11.2020";
+            var expected = "de/fluesse/wasserstand/isar/muenchen-himmelreichbruecke-16515005/messwerte/tabelle?beginn=23.11.2020&ende=24.11.2020";
 
             Assert.AreEqual(expected, actual);
         }
